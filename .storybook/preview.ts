dd1836fa9a1867d918b90e904011fbe0b0b0f3d7
Ahmed Mawia <ahmedmawia.dev@gmail.com>
2025-09-09 07:11:41 +0530

    Updates

---
+++ .storybook/preview.ts
@@ -0,0 +1,21 @@
+import "../src/app/globals.css";
+import type { Preview } from "@storybook/nextjs";
+import { GoogleFontsDecorator } from "./decorators";
+import maw1a from "./themes/maw1a";
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+			docs: {
+				theme: maw1a,
+			},
+		},
+	},
+	decorators: [GoogleFontsDecorator],
+};
+
+export default preview;
---
+++ .storybook/themes/maw1a.ts
@@ -0,0 +1,23 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+	base: "light",
+
+	brandTitle: "UI Story By Maw1a",
+	brandUrl: "https://uistory.maw1a.com",
+	brandImage: "/uistory/logo.svg",
+
+	fontBase: "Geist Mono, monospace",
+	fontCode: "Geist Mono, monospace",
+
+	colorPrimary: "#272727",
+	colorSecondary: "#272727",
+
+	barTextColor: "#272727",
+	barSelectedColor: "#272727",
+	barHoverColor: "#9f9fa9",
+
+	inputBorder: "#272727",
+	inputTextColor: "#272727",
+	inputBorderRadius: 4,
+});
